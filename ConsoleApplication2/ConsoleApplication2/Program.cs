6d1e740e4f1a3fd56b4909cfe90f473159c4bdc6
hobaominh <teamtheweeknd2016@gmail.com>
2019-10-01 15:16:37 +0700

    cc

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            InputInt(array);
+        }
+        public static void InputInt(int[] Arr)
+        {
+            for (int i = 0; i < Arr.Length; i++) 
+            {
+                Console.Write("A[{0}]= " , 1);
+                string str = Console.ReadLine();
+                Arr[i] = int.Parse(str);
+            }
+        }
+    }
+}
