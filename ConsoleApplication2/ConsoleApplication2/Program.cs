aa50548495d45b242d7953426ddfef7d5d65b526
hobaominh <teamtheweeknd2016@gmail.com>
2019-10-01 15:28:13 +0700

    ccc

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
---
+++ ConsoleApplication2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication2;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIscontain5()
+        {
+            int[] Arr1 = new int[] { 1, 2, 3, 4 };
+            bool result0 = Program.Iscontain5(Arr1);
+            Assert.AreEqual(false.result0);
+
+            int[] Arr2 = new int[] { 1, 2, 3, 4 };
+            bool result1 = Program.Iscontain5(Arr2);
+            Assert.AreEqual(true.result1);
+        }
+    }
+}
